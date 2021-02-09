70dd917f32ee25cbea45f45d0f40cc7816e1aef3
unknown <joseph.tindle@snhu.edu>
2021-02-08 22:40:48 -0700

    Angular Tutorial: Learn Angular from scratch step by step

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://nu2hqps4ve.execute-api.us-east-1.amazonaws.com.us-east-1.amazonaws.com'
 };
